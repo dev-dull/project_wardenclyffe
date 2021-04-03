b81e6bfbc2ef9a709bf463ca53d79b43cf517b06
Alastair Drong <adrong@gmail.com>
2021-04-03 07:31:59 -0700

    Got the next step in the auth process working

--- tesla_tracks.py
+++ tesla_tracks.py
@@ -1,60 +1,68 @@
+import json
 from abc import ABC
-
 from CONSTS import C
 from requests import Session
 from html.parser import HTMLParser
 # from requests_oauthlib import OAuth2Session
 
 
-class WardenclyffeHiddenParamParser(HTMLParser, ABC):
-    def __init__(self):
-        super().__init__()
-        self.in_form = False
-        self.hidden_data = {}
+class HiddenDataInForm(dict):
+    # Wrapper class for self._ParseForHiddenData so that usage and accessing data is more intuitive
+    def __init__(self, html):
+        pfhd = self._ParseForHiddenData()
+        pfhd.feed(html)
+        self.update(pfhd.hidden_data)
 
-    def handle_starttag(self, tag, attrs):
-        d_attrs = dict(attrs)
-        if str(tag) == 'form':
-            self.in_form = True
-            print('yup!')
-        elif str(tag) == 'input' and self.in_form and d_attrs.get('type', '').lower() == 'hidden':
-            if 'name' in d_attrs and 'value' in d_attrs:
-                self.hidden_data[d_attrs['name']] = d_attrs['value']
-                print('name: %s' % d_attrs['name'], 'value: %s' % d_attrs['value'])
+    class _ParseForHiddenData(HTMLParser, ABC):
+        def __init__(self):
+            super().__init__()
+            self.in_form = False
+            self.hidden_data = {}
 
-    def handle_endtag(self, tag):
-        pass
-        # print("Encountered an end tag :", tag)
+        def handle_starttag(self, tag, attrs):
+            d_attrs = dict(attrs)
+            if str(tag) == 'form':
+                self.in_form = True
+            elif str(tag) == 'input' and self.in_form and d_attrs.get('type', '').lower() == 'hidden':
+                if 'name' in d_attrs and 'value' in d_attrs:
+                    self.hidden_data[d_attrs['name']] = d_attrs['value']
 
-    def handle_data(self, data):
-        pass
-        # print("Encountered some data  :", data)
+        def handle_endtag(self, tag):
+            if str(tag) == 'form':
+                self.in_form = False
+
+        def handle_data(self, data):
+            # This function is here for future reference.
+            pass
 
 
 class ProjectWardenclyffe(Session):
-    def __init__(self, username, password, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(ProjectWardenclyffe, self).__init__()
 
-        self.username = username
-        self.password = password
-
     def authenticate(self):
-        p = WardenclyffeHiddenParamParser()
-        # TODO: consider using `from requests_oauthlib import OAuth2Session` instead.
-        r = self.get(C.AUTH_BASE_URL + C.AUTH_ENDPOINT_V3_AUTHORIZE, params=C.AUTH_INIT_VALUES)
-        print(r.status_code)
-        p.feed(r.text)
-        # oauth = OAuth2Session(client_id=C.HTTP_GET_VALUE_OWNERAPI, scope=('openid', 'email', 'offline_access'),
-        #                       redirect_uri=C.HTTP_GET_VALUE_VOID_CALLBACK)
+        # FUTURE,1: consider using `from requests_oauthlib import OAuth2Session` instead.
+        # TODO,1: Test that response.status_code is in sensible range.
+        # TODO,2: With value for login_hint populated, A 303 code means that you're trying to auth against region A but
+        # TODO,2: user belongs to region B (e.g. you used auth.tesla.com when auth.tesla.cn is needed)
+
+        # Send initial request to get header value and hidden input params required for auth
+        response = self.get(C.AUTH_BASE_URL + C.AUTH_ENDPOINT_V3_AUTHORIZE, params=C.AUTH_INIT_VALUES, headers=C.HEADERS)
+        form_data = HiddenDataInForm(response.text)
+
+        # Update headers/data for next auth step
+        C.HEADERS.update({'Cookie': response.headers['set-cookie']})
+        form_data.update({'identity': C.USERNAME, 'credential': C.PASSWORD})
+
+        # Submit credentials to get an authorization code
+        response = self.post(C.AUTH_BASE_URL + C.AUTH_ENDPOINT_V3_AUTHORIZE, data=json.dumps(form_data),
+                             params=C.AUTH_INIT_VALUES, headers=C.HEADERS)
+        print(response.status_code)
+        print(response.text)
 
 
 def main():
-    # for k,v in C.AUTH_INIT_VALUES.items():
-    #     if isinstance(v, bytes):
-    #         print(k)
-
-
-    pw = ProjectWardenclyffe(C.USERNAME, C.PASSWORD)
+    pw = ProjectWardenclyffe()
     pw.authenticate()
 
 
