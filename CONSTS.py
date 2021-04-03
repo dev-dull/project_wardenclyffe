c1d69492dde9ebd766c445fe14828e979eadcc42
Alastair Drong <adrong@gmail.com>
2021-04-03 06:03:27 -0700

    Successfully getting the initial HTML form back

--- CONSTS.py
+++ CONSTS.py
@@ -1,7 +1,8 @@
+import os
 import yaml
 from hashlib import sha256
 from random import shuffle
-from base64 import b64encode
+from base64 import urlsafe_b64encode
 from string import ascii_letters, digits
 
 # This file hijacks the order in which Python parses the file in order to create user-configurable items.
@@ -81,13 +82,23 @@ C.HTTP_GET_VALUE_VOID_CALLBACK = C.AUTH_BASE_URL + C.AUTH_ENDPOINT_VOID_CALLBACK
 C.HTTP_GET_VALUE_CODE = 'code'
 C.HTTP_GET_VALUE_OPENID_EMAIL_OFFLINE_ACCESS = 'openid email offline_access'
 
-C.AUTH_CODE_CHARACTER_SET = list(ascii_letters*2 + digits*2)
-shuffle(C.AUTH_CODE_CHARACTER_SET)
-C.AUTH_GET_CODE_VERIFY = ''.join(C.AUTH_CODE_CHARACTER_SET[0:86])
-C.AUTH_GET_CODE_CHALLENGE = b64encode(sha256(C.AUTH_CODE_VERIFY.encode()).hexdigest().encode())
+
+# code_verifier = base64.urlsafe_b64encode(os.urandom(32)).rstrip(b'=')
+# unencoded_digest = hashlib.sha256(code_verifier).digest()
+# code_challenge = base64.urlsafe_b64encode(hashlib.sha256(code_verifier).digest()).rstrip(b'=')
+
+# C.AUTH_CODE_CHARACTER_SET = list(ascii_letters*2 + digits*2)
+# shuffle(C.AUTH_CODE_CHARACTER_SET)
+C.AUTH_GET_CODE_VERIFY = urlsafe_b64encode(os.urandom(32)).rstrip(b'=')
+C.AUTH_GET_CODE_CHALLENGE = urlsafe_b64encode(sha256(C.AUTH_GET_CODE_VERIFY).digest()).rstrip(b'=')
 
 C.AUTH_INIT_VALUES = {
     C.HTTP_GET_KEYWORD_CLIENT_ID: C.HTTP_GET_VALUE_OWNERAPI,
+    C.HTTP_GET_KEYWORD_CODE_CHALLENGE: str(C.AUTH_GET_CODE_CHALLENGE),
     C.HTTP_GET_KEYWORD_CODE_CHALLENGE_METHOD: C.HTTP_GET_VALUE_S256,
-    C.HTTP_GET_KEYWORD_CODE_CHALLENGE: C.AUTH_GET_CODE_CHALLENGE,
+    C.HTTP_GET_KEYWORD_REDIRECT_URI: C.HTTP_GET_VALUE_VOID_CALLBACK,
+    C.HTTP_GET_KEYWORD_RESPONSE_TYPE: C.HTTP_GET_VALUE_CODE,
+    C.HTTP_GET_KEYWORD_SCOPE: C.HTTP_GET_VALUE_OPENID_EMAIL_OFFLINE_ACCESS,
+    C.HTTP_GET_KEYWORD_STATE: 'Any random string?',  # "The OAuth state value. Any random string."
+    C.HTTP_GET_KEYWORD_LOGIN_HINT: C.USERNAME
 }
--- tesla_tracks.py
+++ tesla_tracks.py
@@ -1,8 +1,61 @@
-import requests
+from abc import ABC
+
 from CONSTS import C
+from requests import Session
+from html.parser import HTMLParser
+# from requests_oauthlib import OAuth2Session
+
+
+class WardenclyffeHiddenParamParser(HTMLParser, ABC):
+    def __init__(self):
+        super().__init__()
+        self.in_form = False
+        self.hidden_data = {}
+
+    def handle_starttag(self, tag, attrs):
+        d_attrs = dict(attrs)
+        if str(tag) == 'form':
+            self.in_form = True
+            print('yup!')
+        elif str(tag) == 'input' and self.in_form and d_attrs.get('type', '').lower() == 'hidden':
+            if 'name' in d_attrs and 'value' in d_attrs:
+                self.hidden_data[d_attrs['name']] = d_attrs['value']
+                print('name: %s' % d_attrs['name'], 'value: %s' % d_attrs['value'])
+
+    def handle_endtag(self, tag):
+        pass
+        # print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        pass
+        # print("Encountered some data  :", data)
+
+
+class ProjectWardenclyffe(Session):
+    def __init__(self, username, password, **kwargs):
+        super(ProjectWardenclyffe, self).__init__()
+
+        self.username = username
+        self.password = password
+
+    def authenticate(self):
+        p = WardenclyffeHiddenParamParser()
+        # TODO: consider using `from requests_oauthlib import OAuth2Session` instead.
+        r = self.get(C.AUTH_BASE_URL + C.AUTH_ENDPOINT_V3_AUTHORIZE, params=C.AUTH_INIT_VALUES)
+        print(r.status_code)
+        p.feed(r.text)
+        # oauth = OAuth2Session(client_id=C.HTTP_GET_VALUE_OWNERAPI, scope=('openid', 'email', 'offline_access'),
+        #                       redirect_uri=C.HTTP_GET_VALUE_VOID_CALLBACK)
+
 
 def main():
-    print(C.USERNAME)
+    # for k,v in C.AUTH_INIT_VALUES.items():
+    #     if isinstance(v, bytes):
+    #         print(k)
+
+
+    pw = ProjectWardenclyffe(C.USERNAME, C.PASSWORD)
+    pw.authenticate()
 
 
 if __name__ == '__main__':
